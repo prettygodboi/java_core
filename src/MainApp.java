0ea46ec1ccde9168a26d5715aef8a5dc8f177f41
prettygodboi <dima.atzo@gmail.com>
2023-09-15 20:14:33 +0500

    couple tasks

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,20 +1,61 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainApp {
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.add(1);
-        myLinkedList.add(2);
-        myLinkedList.add(3);
+//        MyLinkedList myLinkedList = new MyLinkedList();
+//        myLinkedList.add(1);
+//        myLinkedList.add(2);
+//        myLinkedList.add(3);
+//
+//        System.out.println(myLinkedList);
+//        System.out.println(myLinkedList.get(1));
+//        System.out.println(myLinkedList.get(0));
+//        System.out.println(myLinkedList.get(2));
+//        myLinkedList.remove(1);
+//        System.out.println(myLinkedList);
+//
+//        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+//        myArrayList.add(2);
+//        System.out.println(myArrayList);
+//
+//        int[] array = new int[]{1,5,3,15,2};
+//        System.out.println(Arrays.stream(array).sorted().collect(Collectors.toList());
 
-        System.out.println(myLinkedList);
-        System.out.println(myLinkedList.get(1));
-        System.out.println(myLinkedList.get(0));
-        System.out.println(myLinkedList.get(2));
-        myLinkedList.remove(1);
-        System.out.println(myLinkedList);
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 && i % 5 != 0 && sum(i)){
+                System.out.println(i);
+            }
 
+        }
+
+//        for(int i = 0; i < 100; i++){
+//            if (i == 0){
+//                continue;
+//            }
+//            if (i % 3 == 0 & i % 5 == 0) {
+//                System.out.println("FizzBuzz" + i);
+//            }
+//            if (i % 3 == 0){
+//                System.out.println("Fizz" + i);
+//            }
+//            if (i % 5 == 0) {
+//                System.out.println("Buzz" + i);
+//            }else {
+//                System.out.println(i);
+//            }
+//        }
+    }
+
+    public static boolean sum(int num){
+        int sum = 0;
+        while (num > 0 & sum < 10){
+            sum += num % 10;
+            num = num/10;
+        }
+        return sum < 10;
     }
 }
---
+++ src/MyArrayList.java
@@ -0,0 +1,40 @@
+import java.io.ObjectStreamClass;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MyArrayList<T> {
+    private static final int INIT_CAPACITY = 10;
+    private Object[] array = new Object[INIT_CAPACITY];
+    private int size = 0;
+    transient Object[] elementData;
+
+    public void add(T item){
+        if (size == array.length - 1){
+            resize(array.length * 2);
+        }
+        array[size++] = item;
+    }
+
+    public T get(int index){
+        return (T) array[index];
+    }
+
+    public void remove(int index){
+        for (int i = index; i < size; i++) {
+            array[i] = array[i + 1];
+
+
+        }
+    }
+
+    public void resize(int newSize){
+        Object[] newArray = new Object[newSize];
+        System.arraycopy(array,0,newArray,0, size);
+        array = newArray;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
+    }
+}
